c092a956f44ef04e1f489a5e548f0b250757c945
Tanuj Pant <tanujpant13@gmail.com>
2026-07-30 18:09:45 +0530

    (feat): add support and connect pieces for observability * open telemetry * collector * tempo * grafana

--- apps/backend/src/observability/telemetry.ts
+++ apps/backend/src/observability/telemetry.ts
@@ -1,13 +1,26 @@
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { NodeSDK } from "@opentelemetry/sdk-node";
+import { resourceFromAttributes } from "@opentelemetry/resources";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
+import {
+  ATTR_DEPLOYMENT_ENVIRONMENT_NAME,
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from "@opentelemetry/semantic-conventions";
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 const exporter = new OTLPTraceExporter({
   url: "http://localhost:4318/v1/traces",
 });
 
 const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "backend",
+    [ATTR_SERVICE_VERSION]: "0.0.1",
+    [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]: "development",
+  }),
   spanProcessors: [new SimpleSpanProcessor(exporter)],
   instrumentations: [
     getNodeAutoInstrumentations({
